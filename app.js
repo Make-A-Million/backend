729bba54c94fbf7737f0c46b95a5639f2c132892
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-11-04 11:52:07 +0530

    configured error handler

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const ErrorMiddleware = require("./middleware/error");
 
 dotenv.config({ path: 'config/config.env' });
+app.use(express.json());
 
+
+
+app.use(ErrorMiddleware);
 module.exports = app;
