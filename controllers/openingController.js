84a4c625f0bd318acaa20827fa253e1d241bedd7
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-11-04 14:34:15 +0530

    created routes for get all openings, get single openings, get all company openings

--- controllers/openingController.js
+++ controllers/openingController.js
@@ -21,3 +21,42 @@ exports.newOpening = CatchAsyncErrors(async (req, res, next) => {
         opening
     })
 });
+
+// Get all openings => /api/v1/openings
+exports.getOpenings = CatchAsyncErrors(async (req, res, next) => {
+    const openings = await Opening.find();
+
+    res.status(200).json({
+        success: true,
+        openings
+    })
+});
+
+// Get single opening => /api/v1/openings/:id
+exports.getSingleOpening = CatchAsyncErrors(async (req, res, next) => {
+    const opening = await Opening.findById(req.params.id);
+
+    if(!opening)
+        return next(new ErrorHandler("Opening not found", 404));
+
+    res.status(200).json({
+        success: true,
+        opening
+    })
+});
+
+//get all openings of a company
+exports.getCompanyOpenings = CatchAsyncErrors(async (req, res, next) => {
+    const id = req.params.id;
+    const company = await Company.findById(id);
+
+    if (!company)
+        return next(new ErrorHandler("Company not found", 404));
+
+    const openings = await Opening.find({company: id});
+
+    res.status(200).json({
+        success: true,
+        openings
+    });
+})
--- routes/openingRoutes.js
+++ routes/openingRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const Router = express.Router();
-const {newOpening} = require("../controllers/openingController");
+const {newOpening, getOpenings, getSingleOpening, getCompanyOpenings} = require("../controllers/openingController");
 
 Router.route("/:id/new").post(newOpening);
+Router.route("/all").get(getOpenings);
+Router.route("/:id").get(getSingleOpening);
+Router.route("/company/:id").get(getCompanyOpenings);
+
 
 module.exports = Router;
